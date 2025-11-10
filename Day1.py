dd60d332f6ccf979f2d75161bb222911ef303377
LawCrust <138283678+DarkWizardCK-24@users.noreply.github.com>
2025-11-10 16:21:28 +0530

    Initial commit

---
+++ Day1.py
@@ -0,0 +1,16 @@
+# print("Hello, World!")     # Output: Hello, World!
+# print('Welcome to Python') # Strings in quotes
+
+name = "Chaitanya"
+age = 21
+is_active = True
+print(name, age, is_active)
+
+user_name = input("Enter your name: ")
+print("Hello", user_name)
+
+print(type(name))  # <class 'str'>
+print(type(age))   # <class 'int'>
+
+print(f"My name is {name} and I am {age} years old.")
+
